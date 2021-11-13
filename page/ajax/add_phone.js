dc6583db9c9af06f12f462b6c918c12c254842df
gotnatsarin <81910303+gotnatsarin@users.noreply.github.com>
2021-11-13 15:46:07 +0700

    test

--- page/ajax/add_phone.js
+++ page/ajax/add_phone.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
     var message = "กรุณาระบุข้อมูลให้ครบ";
+    var phonenum = $('#room_name').val();
+    var status = $('#status').val();
+
+
+    $('#clear').click(function() {
+        $("#myform").trigger('reset');
+    })
 
     $('#save').click(function() {
-        var phonenum = $('#room_name').val();
-        var status = $('#status').val();
+
 
     
     })
